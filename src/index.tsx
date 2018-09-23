54fa3e8464264e0190e491eaf91c540ef958901e
Res Yeg <shaqk702@gmail.com>
2018-09-22 18:03:13 -0700

    added redux stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 // import App from './App';
-import Hello from './components/Hello';
+// import Hello from './components/Hello';
 // import Hello from './components/StatefulHello';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+import { EnthusiasmAction } from './actions/index';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
+
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
